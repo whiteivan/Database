ddb8708379b4a5b9f0971999d446d3e76c6052dd
Danil <beloivanenkodanil@gmail.com>
2024-07-29 20:16:25 +0600

    add main header file

---
+++ declaration.h
@@ -0,0 +1,9 @@
+static int32_t read_full(int fd, char* buf, size_t n);
+static int32_t one_request(int connfd);
+static void die(const char *msg);
+static int32_t write_all (int fd, char* buf, size_t n);
+static void msg(const char *msg);
+static void die(const char *msg);
+static void do_something(int connfd);
+
+
