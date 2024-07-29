5157feb03ad8f698a84745109615306b33ffcc21
Danil <beloivanenkodanil@gmail.com>
2024-07-29 21:12:50 +0600

    add query func in client.cpp

--- client.cpp
+++ client.cpp
@@ -7,13 +7,57 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
-
+#include "declaration.h"
 
 static void die(const char *msg) {
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
 }
+
+static int32_t query(int fd, const char* text){
+    uint32_t len = (uint32_t)strlen(text);
+    if(len > k_max_msg){
+        return -1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write(fd, wbuf, 4 + len)){
+        return err;
+    }
+
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if(err){
+        if(errno == 0){
+            msg("EOF");
+        } else{
+            msg("read() error");
+        }
+        return err;
+    }
+
+    memcpy(&len, rbuf, 4);
+    if(len > k_max_msg){
+        msg("too long");
+        return -1;
+    }
+
+    err = read_full(fd, &rbuf[4], len);
+    if(err){
+        msg("read() error");
+        return err;
+    }
+
+    rbuf[4 + len] = '\0';
+    printf("server says: %s\n", &rbuf[4]);
+    return 0;
+}
+
+
 
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
