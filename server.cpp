97042d0e34a90a22a25e14e66dc91926a60a2e41
Danil <beloivanenkodanil@gmail.com>
2024-07-29 19:10:24 +0600

    add Output helper func

--- server.cpp
+++ server.cpp
@@ -23,7 +23,18 @@ static int32_t read_full(int fd, char* buf, size_t n){
     return 0;
 }
 
-static int32_t write_all
+static int32_t write_all (int fd, char* buf, size_t n){
+    while(n > 0){
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
