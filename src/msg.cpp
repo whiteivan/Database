c99e2319093ceb204fd3a9025a9e072933881bef
Danil <beloivanenkodanil@gmail.com>
2024-08-12 21:33:43 +0600

    split and add def & decl of msg & die func

---
+++ src/msg.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <errno.h>
+#include <cstdlib>
+
+void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
