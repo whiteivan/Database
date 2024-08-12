3a3c9da77290385543a62a8d9127372c44edfe81
Danil <beloivanenkodanil@gmail.com>
2024-08-12 22:03:12 +0600

    split query func, add query.cpp & query.h

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,58 +1,10 @@
-#include "../include/write_all.h"
-#include "../include/read_full.h"
-#include "../include/msg.h"
-#include <arpa/inet.h>    // for ntohl, ntohs
-#include <errno.h>        // for errno
-#include <netinet/in.h>   // for sockaddr_in, INADDR_LOOPBACK, in_addr
-#include <stdint.h>       // for int32_t, uint32_t
-#include <stdio.h>        // for printf
-#include <string.h>       // for memcpy, strlen
-#include <sys/socket.h>   // for AF_INET, connect, socket, SOCK_STREAM
-#include <unistd.h>       // for close
-#include "../include/consts.h"
-
-static int32_t query(int fd, const char* text){
-    uint32_t len = (uint32_t)strlen(text);
-    if(len > k_max_msg){
-        return -1;
-    }
-
-    char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4);
-    memcpy(&wbuf[4], text, len);
-    if (int32_t err = write_all(fd, wbuf, 4 + len)){
-        return err;
-    }
-
-    char rbuf[4 + k_max_msg + 1];
-    errno = 0;
-    int32_t err = read_full(fd, rbuf, 4);
-    if(err){
-        if(errno == 0){
-            msg("EOF");
-        } else{
-            msg("read() error");
-        }
-        return err;
-    }
-
-    memcpy(&len, rbuf, 4);
-    if(len > k_max_msg){
-        msg("too long");
-        return -1;
-    }
-
-    err = read_full(fd, &rbuf[4], len);
-    if(err){
-        msg("read() error");
-        return err;
-    }
-
-    rbuf[4 + len] = '\0';
-    printf("server says: %s\n", &rbuf[4]);
-    return 0;
-}
-
+#include <arpa/inet.h>         // for ntohl, ntohs
+#include <netinet/in.h>        // for sockaddr_in, INADDR_LOOPBACK, in_addr
+#include <stdint.h>            // for int32_t
+#include <sys/socket.h>        // for AF_INET, connect, socket, SOCK_STREAM
+#include <unistd.h>            // for close
+#include "../include/msg.h"    // for msg, die
+#include "../include/query.h"  // for query
 
 
 int main() {
---
+++ src/include/query.h
@@ -0,0 +1,3 @@
+#pragma once 
+#include <stdint.h>
+int32_t query(int fd, const char* text);
