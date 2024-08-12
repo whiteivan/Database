73d409e7eb7253ebf0d48c3f59629972e03b121f
Danil <beloivanenkodanil@gmail.com>
2024-08-12 21:41:15 +0600

    move server.cpp in another dir & fix includes by iwyu

---
+++ src/server/server.cpp
@@ -0,0 +1,98 @@
+#include <arpa/inet.h>             // for ntohl, ntohs
+#include <errno.h>                 // for errno
+#include <netinet/in.h>            // for sockaddr_in, in_addr
+#include <stdint.h>                // for int32_t, uint32_t
+#include <stdio.h>                 // for printf
+#include <string.h>                // for memcpy, strlen
+#include <sys/socket.h>            // for AF_INET, accept, bind, listen, set...
+#include <unistd.h>                // for close
+#include "../include/consts.h"     // for k_max_msg
+#include "../include/msg.h"        // for msg, die
+#include "../include/read_full.h"  // for read_full
+#include "../include/write_all.h"  // for write_all
+
+
+
+static int32_t one_request(int connfd){
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if(err){
+        if (errno == 0) {
+            msg("EOF");
+        } else{
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf,4);
+    if(len > k_max_msg){
+        msg("too long");
+        return -1;
+    }
+
+    err = read_full(connfd, &rbuf[4], len);
+    if(err){
+        msg("read() error");
+        return err;
+    }
+    
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);//do_something
+    
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    // this is needed for most server applications
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    // bind
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
+    int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("bind()");
+    }
+
+    // listen
+    rv = listen(fd, SOMAXCONN);
+    if (rv) {
+        die("listen()");
+    }
+
+    while (true) {
+        // accept
+        struct sockaddr_in client_addr = {};
+        socklen_t socklen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+        if (connfd < 0) {
+            continue;   // error
+        }
+        
+        while (true) {
+            int32_t err = one_request(connfd);//only one client connection at once
+            if (err) {
+                break;
+            }
+        }
+        close(connfd);
+    }
+
+    return 0;
+}
